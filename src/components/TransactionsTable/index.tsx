a90a49fcfbe91ca6c86fb42b913695eb7b050925
Artur Ribeiro <profissional.arturrc@gmail.com>
2021-08-26 09:30:42 -0300

    Artmoney, estrutura inicial

---
+++ src/components/TransactionsTable/index.tsx
@@ -0,0 +1,38 @@
+import { useEffect } from "react";
+import { api } from "../../services/api";
+import { Container } from "./styles";
+
+export function TransactionsTable() {
+  useEffect(() => {
+    api.get("transactions").then((response) => console.log(response.data));
+  }, []);
+
+  return (
+    <Container>
+      <table>
+        <thead>
+          <tr>
+            <th>Título</th>
+            <th>Preço</th>
+            <th>Categoria</th>
+            <th>Data</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>Desenvolvimento Web</td>
+            <td className="deposit">R$12.000</td>
+            <td>Desenvolvimento</td>
+            <td>17/11/1989</td>
+          </tr>
+          <tr>
+            <td>Desenvolvimento Web</td>
+            <td className="withdraw">- R$12.000</td>
+            <td>Alimentação</td>
+            <td>23/02/1961</td>
+          </tr>
+        </tbody>
+      </table>
+    </Container>
+  );
+}
