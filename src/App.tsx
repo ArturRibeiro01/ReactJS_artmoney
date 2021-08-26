3f62340eace5d197e0a4ccc774951a4cb87ee750
Artur Ribeiro <profissional.arturrc@gmail.com>
2021-08-26 18:46:59 -0300

    Criação do componente modal

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,33 @@
 import { GlobalStyle } from "./styles/global";
+import { NewTransactionModal } from "./components/NewTransactionModal";
 import { Header } from "./components/Header";
 import { Dashboard } from "./components/Dashboard";
+import Modal from "react-modal";
+import { useState } from "react";
+
+Modal.setAppElement("#root");
 
 export function App() {
+  // Comportamentode abertura e fechamento do modal
+  const [isNewTransationModalOpen, setIsNewTransationModalOpen] =
+    useState(false);
+
+  function handleOpenNewTransactionModal() {
+    setIsNewTransationModalOpen(true);
+  }
+  function handleCloseNewTransactionModal() {
+    setIsNewTransationModalOpen(false);
+  }
   return (
     <>
-      <Header />
+      <Header onOpenNewTransactionModal={handleOpenNewTransactionModal} />
       <Dashboard />
+
+      <NewTransactionModal
+        isOpen={isNewTransationModalOpen}
+        onRequestClose={handleCloseNewTransactionModal}
+      />
+
       <GlobalStyle />
     </>
   );
---
+++ src/components/NewTransactionModal/index.tsx
@@ -0,0 +1,30 @@
+import { Container } from "./styles";
+import Modal from "react-modal";
+
+interface NewTransactionModalProps {
+  isOpen: boolean;
+  onRequestClose: () => void;
+}
+
+export function NewTransactionModal({
+  isOpen,
+  onRequestClose,
+}: NewTransactionModalProps) {
+  return (
+    <Modal
+      isOpen={isOpen}
+      onRequestClose={onRequestClose}
+      overlayClassName="react-modal-overlay"
+      className="react-modal-content"
+    >
+      <Container>
+        <h2> Cadastrar Transação</h2>
+
+        <input type="text" placeholder="Titulo" />
+        <input type="number" placeholder="Valor" />
+        <input type="text" placeholder="Categoria" />
+        <button type="submit">Cadastrar</button>
+      </Container>
+    </Modal>
+  );
+}
